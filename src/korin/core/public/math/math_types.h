0c404eb7cdbfdca9ac051958b44f9f2716a52315
sneppy <andrea.mecchia@mail.polimi.it>
2021-04-29 15:09:45 +0200

    Added basic math types.

---
+++ src/korin/core/public/math/math_types.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "core_types.h"
+
+namespace Math
+{
+	template<typename> struct Vec2;
+	template<typename> struct Vec3;
+	template<typename> struct Vec4;
+	template<typename> struct Mat3;
+	template<typename> struct Mat4;
+					   struct Quat;
+					   struct TransformMatrix;
+					   struct TranslationAndRotationMatrix;
+					   struct RotationMatrix;
+					   struct PerspectiveProjectionMatrix;
+					   struct OrthographicProjectionMatrix;
+} // namespace Math
+
+using vec2 = Math::Vec2<float32>;
+using vec3 = Math::Vec3<float32>;
+using vec4 = Math::Vec4<float32>;
+
+using float2 = Math::Vec2<float32>;
+using float3 = Math::Vec3<float32>;
+using float4 = Math::Vec4<float32>;
+
+using int2 = Math::Vec2<int32>;
+using int3 = Math::Vec3<int32>;
+using int4 = Math::Vec4<int32>;
+
+using uint2 = Math::Vec2<uint32>;
+using uint3 = Math::Vec3<uint32>;
+using uint4 = Math::Vec4<uint32>;
+
+using mat3 = Math::Mat3<float32>;
+using mat4 = Math::Mat4<float32>;
+
+using quat = Math::Quat;
