e614999ebee8e0c48b7f55f967228752546134d8
Andrea Mecchia <andrea.mecchia@mail.polimi.it>
2021-09-28 15:28:02 +0100

    Replace ASSERT* and CHECK* with KORIN_ASSERT* and KORIN_CHECK*.

--- src/korin/core/public/misc/logging.h
+++ src/korin/core/public/misc/logging.h
@@ -6,22 +6,29 @@
 #define TERM_COLOR_BRIGHT_RED "\u001b[31;1m"
 #define TERM_COLOR_BRIGHT_YELLOW "\u001b[33;1m"
 
+// TODO: Change to KORIN_ASSERT* and KORIN_CHECK*
 #if BUILD_RELEASE
-#	define ASSERTF(x, fmt, ...)
-#	define CHECKF(x, fmt, ...)
+#	define KORIN_ASSERTF(x, fmt, ...)
+#	define KORIN_CHECKF(x, fmt, ...)
 #else
-#	define ASSERTF(x, fmt, ...)\
+#	define KORIN_ASSERTF(x, fmt, ...)\
 		if (UNLIKELY(!(x)))\
 		{\
 			fprintf(stderr, "%s:%d:" TERM_COLOR_BRIGHT_RED " error:" TERM_COLOR_RESET " " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);\
 			exit(1);\
 		}
-#	define CHECKF(x, fmt, ...)\
+#	define KORIN_CHECKF(x, fmt, ...)\
 		if (UNLIKELY(!(x)))\
 		{\
 			fprintf(stderr, "%s:%d:" TERM_COLOR_BRIGHT_YELLOW " warning:" TERM_COLOR_RESET " " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);\
 		}
 #endif
 
-#define ASSERT(x) ASSERTF("ASSERT failed: %s.", #x)
-#define CHECK(x) CHECKF("CHECK failed: %s.", #x)
+#define KORIN_ASSERT(x) KORIN_ASSERTF("ASSERT failed: %s.", #x)
+#define KORIN_CHECK(x) KORIN_CHECKF("CHECK failed: %s.", #x)
+
+// This are deprecated
+#define ASSERTF(...) KORIN_ASSERTF(__VA_ARGS__)
+#define ASSERT(...) KORIN_ASSERT(__VA_ARGS__)
+#define CHECKF(...) KORIN_CHECKF(__VA_ARGS__)
+#define CHECK(...) KORIN_CHECK(__VA_ARGS__)
